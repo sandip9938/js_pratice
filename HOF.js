cb82aded5893b74288a92e92b485d665309c50ef
sandip9938 <sandippanda577@gmail.com>
2023-08-10 13:28:32 +0530

    js_pratice

--- HOF.js
+++ HOF.js
@@ -5,3 +5,17 @@ const tarr=[2,3,4,5]
 
 const tharr=[...oarr,...tarr]
 console.log(tharr);
+
+
+
+// clousre 
+let score=10
+function name() {
+    let score = 0
+    console.log(score); 
+    // Inner scope 
+}
+name()
+console.log(score);
+// Outer scope 
+
