b943adcbb80769f9461926c4d3b45b6066ee6ec3
sandip9938 <sandippanda577@gmail.com>
2023-08-21 20:49:17 +0530

    js_pratice

--- react_hiteshsir/src/App.jsx
+++ react_hiteshsir/src/App.jsx
@@ -1,8 +1,39 @@
+// hooks concept
 
-function App(){
+import { useState } from "react";
+function App() {
+  let [counter, setCount] = useState(20);
+  // let counter=20
+
+
+
+  // addvalue 
+
+  const addValue = () => {
+    // counter = counter+1
+    setCount(counter + 1);
+    if(setCount>20){
+      alert('its last number')
+    }
+  };
+
+
+
+// removeValue 
+
+  const removeValue = () => {
+    setCount(counter - 1);
+  };
   return (
-    <h1>Hello react</h1>
-  )
+    <>
+      <h1>Hello react</h1>
+      <h2>counter value: {counter}</h2>
+      <button onClick={addValue}>Add value</button>
+      <button onClick={removeValue}>Remove value</button>
+    </>
+
+    // *<button onClick={() => setCount(count + 1)}>+</button>*/
+  );
 }
 
-export default App
+export default App;
