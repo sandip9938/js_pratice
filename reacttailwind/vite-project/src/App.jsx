75637086abc5df660c146352ccd5693e251395f0
sandip9938 <sandippanda577@gmail.com>
2023-08-25 23:23:20 +0530

    js_pratice

---
+++ reacttailwind/vite-project/src/App.jsx
@@ -0,0 +1,22 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import Imag from './assets/Imag.jsx'
+
+function App() {
+  // const [count, setCount] = useState(0)
+  
+  
+  return (
+    <>
+     <h1 className="heading col-auto bg-orange-600">hii</h1>
+     <button className="rounded-full ...">Save Changes</button>
+
+  <Imag name="Sandip" />
+
+   </>
+  )
+}
+
+export default App
---
+++ reacttailwind/vite-project/src/assets/Imag.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function imag(props) {
+  console.log("props",props);
+  console.log(props.name);
+  return (
+  <>
+  <h1 className='head'
+ >Hello</h1><br/>
+    <img src="https://imgs.search.brave.com/ngjfI0x-ur_V-G4RaakmYL7szAVHJDNqZC3Bg80b5sA/rs:fit:500:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzAzLzE1LzExLzE1/LzM2MF9GXzMxNTEx/MTUzNl82TnR4YXR3/NzBnZXVQRE1QRFJr/WXJXUEdjQXN2V1Bi/VC5qcGc"   alt="" />
+   
+  </>
+  )
+}
+
+
+export default imag
