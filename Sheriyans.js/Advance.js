4400ce5717c600abf8c059bdb74b09da06dcf0d5
Sandip Panda <115527258+sandip9938@users.noreply.github.com>
2023-09-06 00:12:32 +0530

    js_pratice

--- Sheriyans.js/Advance.js
+++ Sheriyans.js/Advance.js
@@ -7,4 +7,13 @@ function callback(data) {
     // Create a new instance of the client with your API key and secret, then call `client.get()` to make an HTTP request:
 
 
-    
+    // HOF 
+    // which function accept another function or return another function its called HOF
+    const add = (a,b)=>{return a+ b;}
+
+    // constructor function 
+    // when normal function is used "this" keyword and when we call this function there used a "new" keyword as lke fuction is constructer function . its used make same propertis many item.
+    let obj1 = new Person("Rahul",25);
+    obj1.greet();
+
+
--- Sheriyans.js/Async.js
+++ Sheriyans.js/Async.js
@@ -14,17 +14,42 @@
 // Asynchronus is a carrying multiple task which is complete automatically after all tasks are completed.
  
 
-
-
-
-
-
 //  2.How to know sync and async data?
+// when we use setInterval,setTimeout,promises,fetch,axios,xmlht its a async. other all data is sync data.
 
-//  3.
-//  4.
-//  5.
-//  6.
+
+//  3.What is async Js 
+// It's an asynchronous programming model that allows you to execute code asynchronously by not blocking the main thread of execution while waiting for something like I/O
+
+
+
+//  4.SingleThreading and multithreading 
+// Single Threading: A single thread can only do one thing at the time. It means that if you have two or more things going on in
+// Multithreading :
+/*
+Multithreading means that the CPU can execute more than one program at once (in parallel). This allows for faster processing of programs by allowing them to
+Multithreading is the ability of a computer system or program to execute two or more instructions at once (in parallel). This allows for faster processing by
+Multitasking means that the CPU can execute more than one program at once. The operating system schedules each process on a separate core or processor
+A process can be divided into two or more threads so that it becomes parallel processing. A thread has the same access to memory space of another thread in.
+
+
+//  5.Callback 
+// Callback function is used for asynchronous programming in javascript. It is also known as an anonymous function or lambda expression. A callback function is passed into another.
+
+
+//  6.async await defination:
+
+
+
+
+
+
+
+
+
+
+
+
 //  7.
 //  8.
 //  9.
