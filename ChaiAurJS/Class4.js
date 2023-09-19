174bd1d1bb19890c11ac0a18245ebb9a51f6b7c1
Sandip Panda <115527258+sandip9938@users.noreply.github.com>
2023-09-19 21:17:13 +0530

    JS_Class4,5

---
+++ ChaiAurJS/Class4.js
@@ -0,0 +1,29 @@
+// Let const var 
+const accountId=376756485492
+let email="sandip@123"
+var accountPassword="12346"
+// accountId=65758659  //its not acceptable due to const keyword.
+accountPassword="3785685"
+email="san@123"
+console.log([accountId,accountPassword,email]);
+
+// let-
+/*
+* It is a block scoped variable and it can be reassigned but cannot be declared again in the same scope.
+*/
+
+// var-
+/*
+*It is also known as global variables because they are accessible from anywhere within your code.
+*But you should avoid using them if possible since their usage may cause unexpected behavior of your application or even crashing of your system.
+*They have been used for years now so we should avoid using them unless absolutely necessary.
+*/
+
+// const-
+/*
+* Const variables must always be initialized when declared otherwise an error will occur.
+* They are immutable which means that once you assign something to a constant then its value never changes throughout runtime of our programme.
+* They are immutable which means that once you initialize their value then you cannot change its reference or data type.
+* They are immutable which means that once assigned you cant change their value or assign new values to them.
+*/
+ 
---
+++ ChaiAurJS/Class5.js
@@ -0,0 +1,27 @@
+// ECMA script 
+'use strict' // Treat all js code newer version
+// window , this, alert,prompt all BOM 
+/* 
+Datatypes-
+1. Primitive Datatype - String Number Boolean undefined null
+2. Object Datatype - Array Function Date RegExp Error Math JSON
+3. Symbol (ES6)
+4. Set Map WeakSet and WeakMap(ES6)
+5. Promises (ES6)
+6. Generator function (ES6)
+7. Async Await (ES6)
+8. Proxy object (ES6)
+9. Reflect API (ES6)
+*/
+
+// primitive datatypes-
+/*
+Integer
+Number
+BigInt
+String
+Boolean
+undefined
+null
+Symbol
+*/
