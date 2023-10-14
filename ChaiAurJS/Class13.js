fee51dd6f8072baf3fa878b1ea6052147b55163b
Sandip Panda <115527258+sandip9938@users.noreply.github.com>
2023-10-14 21:16:14 +0530

    Class13

---
+++ ChaiAurJS/Class13.js
@@ -0,0 +1,14 @@
+let arr=[12,3,4,5,6,78]
+console.log(arr.push(100));
+// console.log("after push",arr);
+
+
+
+// Objects in javascript 
+let Student={
+    nam:'Sandip',
+    age:20,
+    hobby:"Cricket",
+    study:'B.tech',
+}
+console.log(Student.age); 
