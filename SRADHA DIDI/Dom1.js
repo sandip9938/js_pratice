0b5d26cd8a8ea1f0c9bc04b8ae21bc3e834f8a42
Sandip Panda <115527258+sandip9938@users.noreply.github.com>
2023-11-25 22:26:54 +0530

    Dom1

---
+++ SRADHA DIDI/Dom1.js
@@ -0,0 +1,7 @@
+//-----DOM-----
+/* 
+When Web Page is loaded, the browse creates a Document Object Model of the page .  
+The DOM is a programming interface for web documents.
+It represents the structure of the document and allows you to manipulate the content and structure of a document.
+
+*/
